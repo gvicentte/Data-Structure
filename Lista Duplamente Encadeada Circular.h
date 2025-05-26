2abe6e2837b235f25bbc869a57f71ed03ed10634
Gabriel Vicentte <gvicentte10@gmail.com>
2025-05-26 17:15:39 -0300

    Update Lista Duplamente Encadeada Circular.h

--- Lista Duplamente Encadeada Circular.h
+++ Lista Duplamente Encadeada Circular.h
@@ -3,6 +3,7 @@
 
 template <typename T>
 class Node{
+    public:
     Node<T> *next;
     Node<T> *prev;
     T D;
@@ -90,21 +91,29 @@ template <typename T>
 void ListCDE<T>::PushFront(T V){
     Node<T> *P=new Node<T>;
     P->D=V;
-    P->next=Head;
-    P->prev=Head->prev;
-    Head->prev=P;
-    Head=P;
+    if(!Head){
+        P->next=P->prev=P;
+        Head=P;
+    }
+    else{
+        P->next=Head;
+        P->prev=Head->prev;
+        (Head->prev)->next=P;
+        Head->prev=P;
+        Head=P;
+    }
     n++;
 }
+
 
 template <typename T>
 void ListCDE<T>::PushBack(T V){
     Node<T> *P=new Node<T>;
     P->D=V;
-    P->next=Tail->next;
-    P->prev=Tail;
-    Tail->next=P;
-    Tail=P;
+    P->next=Head;
+    P->prev=Head->prev;
+    (Head->prev)->next=P;
+    Head->prev=P;
     n++;
 }
 
@@ -114,12 +123,10 @@ void ListCDE<T>::PushbIt(T V){
     P->D=V;
     P->next=It;
     P->prev=It->prev;
-    if(It->prev){
-        (It->prev)->next=P;
-    }
-    else{
+    if(It==Head){
         Head=P;
     }
+    (It->prev)->next=P;
     It->prev=P;
     n++;
 }
@@ -130,56 +137,81 @@ void ListCDE<T>::PushaIt(T V){
     P->D=V;
     P->prev=It;
     P->next=It->next;
-    if(It->next){
-        (It->next)->prev=P;
-    }
-    else{
-        Tail=P;
-    }
+    (It->next)->prev=P;
     It->next=P;
     n++;
 }
 
 template <typename T>
 void ListCDE<T>::PopIt(){
-    Node<T> *P=It;
-    It=It->next;
-    delete P;
-    if(!Head){
-        Head=P;
+    if(!It){
+        return;
     }
+    Node<T> *P = It;
+    if(n==1){
+        Head=It=nullptr;
+    }
+    else{
+        It=It->next;
+        P->prev->next=P->next;
+        P->next->prev=P->prev;
+        if(Head==P){
+            Head=P->next;
+        }
+    }
+    delete P;
     n--;
 }
+
 
 template <typename T>
 void ListCDE<T>::PopBack(){
-    Node<T> *P=Head->prev;
-    (Head->prev)->next=Head;
-    delete P;
-    if(!It){
-        It=Head;
+    if(!Head){
+        return;
     }
+    Node<T> *P=Head->prev;
+    if(n==1){
+        Head=It=nullptr;
+    }
+    else{
+        P->prev->next=Head;
+        Head->prev=P->prev;
+        if(It==P){
+            It=Head;
+        }
+    }
+    delete P;
     n--;
 }
 
+
 template <typename T>
 void ListCDE<T>::PopFront(){
+    if(!Head){
+        return;
+    }
     Node<T> *P=Head;
-    Head=Head->next;
+    if(n==1){
+        Head=It=nullptr;
+    }
+    else{
+        Head=Head->next;
+        P->prev->next=Head;
+        Head->prev=P->prev;
+        if(It==P){
+            It=Head;
+        }
+    }
+
     delete P;
-    if(!Tail){
-        Tail=Head;
-    }
-    if(!It){
-        It=Head;
-    }
     n--;
 }
+
 
 template <typename T>
 void ListCDE<T>::Clear(){
     Node<T> *P;
-    while(n){
+    while(n>0){
         P=Head;
         Head=Head->next;
         delete P;
